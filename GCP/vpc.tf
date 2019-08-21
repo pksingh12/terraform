59ba4a7684f85f3209a1ab0413fec42e24115397
Prakash Singh <prakashsingh@Prakashs-MacBook-Pro.local>
2019-08-21 23:02:07 +0530

    my changes

--- GCP/vpc.tf
+++ GCP/vpc.tf
@@ -1,6 +1,8 @@
+/*
 resource "google_compute_network" "mynetwork" {
   name                    = "${var.nw_name}"
   auto_create_subnetworks = "${var.nw_flag_subnet_autocreate}"
   routing_mode            = "${var.nw_routing_mode}"
   project                 = "${var.project_id}"
 }
+*/
