c58d6c644269de77a7153ea107a9f23239763340
pksingh12 <36686815+pksingh12@users.noreply.github.com>
2019-08-21 20:19:13 +0530

    Update provider.tf

--- GCP/provider.tf
+++ GCP/provider.tf
@@ -1,6 +1,6 @@
 # Specify the provider (GCP, AWS, Azure)
 provider "google" {
-credentials = "${file("/root/keys/gcp/gcp_test_mydemo_serviceaccount.json")}"
+credentials = "${file("/root/gcp_keys/gcp_key.json")}"
 project = "my-demo-228617"
 region = "us-central1"
 }
